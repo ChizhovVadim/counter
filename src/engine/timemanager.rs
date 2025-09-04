65424e56e14f7804ccf21313e1147a620cba6874
Vadim Chizhov <chizhov.vadim@gmail.com>
2025-09-04 09:13:30 +0300

    implement TimeManager

--- src/engine/timemanager.rs
+++ src/engine/timemanager.rs
@@ -1,40 +1,89 @@
 use crate::chess::Side;
-use crate::domain::{CancelToken, LimitsType, SearchInfo, TournamentLimit};
+use crate::domain::{CancelToken, LimitsType, SearchInfo};
 use std::time::{Duration, Instant};
 
 pub struct TimeManager {
-    start: Instant,
-    limits: LimitsType,
     cancel: CancelToken,
+    start: Instant,
+    fixed_nodes: Option<u64>,
+    fixed_depth: Option<u32>,
+    max_usage: Option<Duration>,
+    ideal_usage: Option<Duration>,
 }
 
 impl Default for TimeManager {
     fn default() -> Self {
-        TimeManager::new(LimitsType::default(), CancelToken::new(), Side::WHITE)
+        TimeManager {
+            cancel: CancelToken::new(),
+            start: Instant::now(),
+            fixed_nodes: None,
+            fixed_depth: None,
+            max_usage: None,
+            ideal_usage: None,
+        }
     }
 }
 
 impl TimeManager {
-    pub fn new(mut limits: LimitsType, cancel: CancelToken, side: Side) -> Self {
-        // temporary use fixed time
-        if limits.tournament.white_time.is_some() || limits.tournament.black_time.is_some() {
-            limits = LimitsType::fixed_time(calc_fixed_time(limits.tournament, side))
+    pub fn new(limits: LimitsType, cancel: CancelToken, side: Side) -> Self {
+        let start = Instant::now();
+
+        let tl = limits.tournament;
+        if tl.white_time.is_none() || tl.black_time.is_none() {
+            return TimeManager {
+                cancel,
+                start,
+                fixed_depth: limits.fixed_depth,
+                fixed_nodes: limits.fixed_nodes,
+                max_usage: limits.fixed_time,
+                ideal_usage: None,
+            };
         }
 
+        let (main, inc) = if side == Side::WHITE {
+            (tl.white_time.unwrap(), tl.white_increment)
+        } else {
+            (tl.black_time.unwrap(), tl.black_increment)
+        };
+
+        let reserve = (main / 20).clamp(100, 1_000);
+        let main = (main - reserve).max(0);
+
+        let (max_usage, ideal_usage) = if let Some(moves) = tl.moves {
+            let mut moves = moves as u64;
+            if moves > 1 {
+                //moves = moves + 5;
+                moves = moves + (moves * 4 / 10).max(5);
+                moves = moves.min(50);
+            }
+            let ideal_usage = main / moves + inc;
+            let max_usage = 5 * main / moves + inc;
+            (max_usage, ideal_usage)
+        } else {
+            let ideal_usage = main / 50 + inc;
+            let max_usage = main / 10 + inc;
+            (max_usage, ideal_usage)
+        };
+        let ideal_usage = ideal_usage.clamp(0, main);
+        let max_usage = max_usage.clamp(0, main);
+
         return TimeManager {
-            start: Instant::now(),
-            limits: limits,
             cancel,
+            start,
+            fixed_depth: None,
+            fixed_nodes: None,
+            max_usage: Some(Duration::from_millis(max_usage)),
+            ideal_usage: Some(Duration::from_millis(ideal_usage)),
         };
     }
 
     pub fn check_timeout(&self, nodes: u64) -> bool {
-        if let Some(fixed_time) = self.limits.fixed_time {
-            if self.start.elapsed() >= fixed_time {
+        if let Some(max_usage) = self.max_usage {
+            if self.start.elapsed() >= max_usage {
                 self.cancel.cancel();
             }
         }
-        if let Some(fixed_nodes) = self.limits.fixed_nodes {
+        if let Some(fixed_nodes) = self.fixed_nodes {
             if nodes >= fixed_nodes {
                 self.cancel.cancel();
             }
@@ -47,31 +96,15 @@ impl TimeManager {
     }
 
     pub fn iteration_complete(&mut self, si: &SearchInfo) {
-        if self.limits.infinite {
-            return;
+        if let Some(ideal_usage) = self.ideal_usage {
+            if self.start.elapsed() >= ideal_usage {
+                self.cancel.cancel();
+            }
         }
-        if let Some(fixed_depth) = self.limits.fixed_depth {
+        if let Some(fixed_depth) = self.fixed_depth {
             if si.depth >= fixed_depth as usize {
                 self.cancel.cancel();
             }
         }
     }
-}
-
-fn calc_fixed_time(tl: TournamentLimit, side: Side) -> Duration {
-    let (main, inc) = if side == Side::WHITE {
-        (tl.white_time.unwrap(), tl.white_increment)
-    } else {
-        (tl.black_time.unwrap(), tl.black_increment)
-    };
-    const DEFAULT_MOVES_TO_GO: u32 = 40;
-    let moves = tl
-        .moves
-        .unwrap_or(DEFAULT_MOVES_TO_GO)
-        .min(DEFAULT_MOVES_TO_GO) as u64;
-    let reserve = (main / 20).clamp(100, 1_000);
-    let main = (main - reserve).max(0);
-    let total = inc + (main - inc) / moves;
-    let total = total.clamp(0, main);
-    return Duration::from_millis(total);
 }
